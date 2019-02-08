25fcf0634ce7cf4a41fe3515f012ec720a63614a
Tyler Blood <tylerblood@tylers-macbook-pro.local>
2019-02-07 23:18:28 -0600

    Initial Commit

---
+++ SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Tyler Blood on 2/7/19.
+//  Copyright © 2019 Tyler Blood. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
